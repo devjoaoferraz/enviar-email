97120871b4708e51ba90bb0659336bed8b45b0b2
João <joaoferraz@hotmail.com>
2020-02-03 16:28:41 -0300

    first commit

---
+++ EnviarEmail/EnviarEmail/Controllers/SendMailerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace EnviarEmail.Controllers
+{
+    public class SendMailerController : Controller
+    {
+        // GET: SendMailer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                //Instância classe email
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+
+                //Instância smtp do servidor, neste caso o gmail.
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("joaoqi@gmail.com", "Segredo2");// Login e senha do e-mail.
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                ModelState.Clear();
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
